7c33373d477e4ca545ce6eedf1a3c7988f8b4918
Flavio Siotto <flavio.siotto@gmail.com>
2023-03-27 17:22:10 +0200

    test per subPath

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': {
+      '/test/api': {
         target: 'http://127.0.0.1:8080',
         changeOrigin: true,
       },
